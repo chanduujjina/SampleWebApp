5738b4a99dc335e6cd98cfb5bf357627f2a597db
chandu Ujjina <chanduujjina129@gmail.com>
2021-07-29 23:36:06 +0530

    Registration and login form

--- src/com/demo/utility/DBUtility.java
+++ src/com/demo/utility/DBUtility.java
@@ -9,7 +9,7 @@ public class DBUtility {
 	
 		public static Connection getConnection() throws SQLException, ClassNotFoundException {
 			 Class.forName("com.mysql.jdbc.Driver");  
-			return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/chandudb", "root", "root");
+			return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/enterprise_db", "root", "root");
 		}
 	
 
